ccd86f5aff08f5cab4d18cfa67ecf54c03addf30
Yousif Shafee <yousif.shafee@nabdacare.com>
2022-06-12 15:38:29 +0200

    Database in Complete mode

---
+++ main/general_fun.py
@@ -0,0 +1,34 @@
+from django.db.models import Max
+import random
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import Advertisement
+
+def get_confirm_code(email):
+    active_code = random.randint(100000, 999999)
+    send_mail(
+        'Verify Email',
+        str(active_code),
+        settings.EMAIL_HOST_USER,
+        [email],
+        fail_silently=True,
+    )
+    return active_code
+
+
+def get_data_by_field(request, search_dict):
+    q = {}
+    for item in request:
+        if request[item] != '' and request[item] != 'NaN':
+            if item in search_dict and search_dict[item]:
+                q[search_dict[item]] = u'%s' % request[item]
+    if 'fprice' not in request or request['fprice'] == 'NaN':
+        fprice = 0
+    else:
+        fprice = request['fprice']
+    if 'lprice' not in request or request['lprice'] == 'NaN':
+        lprice = Advertisement.objects.all().aggregate(Max('price'))['price__max']
+    else:
+        lprice = request['lprice']
+    q[search_dict['price']] = [fprice, lprice]
+    return q
--- main/models.py
+++ main/models.py
@@ -1,25 +1,104 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from multiselectfield import MultiSelectField
 from django.utils import timezone
 from .choices import *
 
 
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True, null=True)
-    email = models.EmailField(max_length=20, unique=True)
+class UserManager(BaseUserManager):
+    def create_user(self, username=None, password=None):
+        if not username:
+            raise ValueError('User must have an Username')
+        user = self.model(username=username)
+        user.set_password(password)
+        user.staff = True
+        user.save(using=self._db)
+        return user
+    
+    def create_vice_delete(self, username=None, password=None):
+        user = self.create_user(username,password=password)
+        user.mission = "Delete"
+        user.admin = False
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+    
+    def create_vice_update(self, username=None, password=None):
+        user = self.create_user(username,password=password)
+        user.mission = "Update"
+        user.admin = False
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+    
+    def create_normal(self, username=None, password=None):
+        user = self.create_user(username=username,password=password)
+        user.mission = "Normal"
+        user.admin = False
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username=None, password=None):
+        user = self.create_user(username=username,password=password)
+        user.mission = "Admin"
+        user.admin = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def get_by_natural_key(self, username):
+        return self.get(username=username)
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=250, unique=True)
     phone = models.CharField(max_length=15, default='')
     first_name = models.CharField(max_length=15, default='')
     last_name = models.CharField(max_length=15, default='')
-    password = models.CharField(max_length=35, default='')
     gender = models.CharField(max_length=10, choices=gender_type, default='')
     mission = models.CharField(max_length=35, choices=action_level, default='Normal')
-    # USERNAME_FIELD = 'username'
-    # REQUIRED_FIELDS = []
-    # objects = UserManager()
+    active_code = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def has_module_perms(self, app_label):
+        if self.mission == "Admin":
+            return True
+        return False
+    
+    def has_perm(self, perm, obj=None):
+        if self.mission == "Admin":
+            return True
+        return False
+    
+    def get_username(self):
+        if self.username:
+            return self.username
+        return self.username
+
+    def get_password(self):
+        if self.password:
+            return self.password
+        return self.username
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.staff
+
+    @property
+    def is_admin(self):
+        if self.mission == "Admin":
+            return True
+        return False
 
     def __str__(self):
-        return self.first_name + ' - ' + self.last_name
+        return self.username
 
 
 class Image(models.Model):
@@ -28,47 +107,50 @@ class Image(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     
     def __str__(self):
-        return self.category + ' - ' + self.created_date.strftime("%m-%d %M:%S")
+        return self.category + ' - ' + str(self.images)
 
 
 class Advertisement(models.Model):
     ad_image = models.ForeignKey(Image, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     ad_name = models.CharField(max_length=200, default='')
-    ad_type = models.CharField(choices=sale_or_rent, max_length=25, default='')
     amount = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
     created_date = models.DateTimeField(default=timezone.now)
-    
+    description = models.TextField(max_length=4096, default='')
+    payment_n = models.CharField(max_length=200, default='', blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+
     def __str__(self):
-        return str(self.user) + ' - ' + self.ad_name
+        return str(self.user.email) + ' - ' + self.ad_name
 
 
 class Car_Brand(models.Model):
-    brnad = models.CharField(max_length=35, choices=car_brand, default='')
+    brand = models.CharField(max_length=35, choices=car_brand, default='')
     model = models.CharField(max_length=35, default='')
     
     def __str__(self):
-        return self.brnad + ' - ' + self.model
+        return self.brand + ' - ' + self.model
 
 
 class Car(models.Model):
     body_type = models.CharField(max_length=35, default='', choices=body_c)
     brand = models.ForeignKey(Car_Brand, on_delete=models.CASCADE, null=True)
     fuel_type = models.CharField(max_length=35, default='', choices=fuel_c)
-    engine_capacity = models.CharField(max_length=35, default='', choices=engin_c)
+    engine_capacity = models.IntegerField(default=0)
     year = models.DateField()
     color = models.CharField(max_length=35, default='', choices=color_c)
     transmission_type = models.CharField(max_length=35, default='', choices=transmission_c)
+    condition = models.CharField(max_length=35, default='', choices=condition_c)
     extra_features = MultiSelectField(choices=extra_c)
     
     def __str__(self):
-        return str(self.brnad) + ' - ' + self.year
+        return str(self.brand) + ' - ' + str(self.year.year)
 
 
 class Car_Rent(models.Model):
     ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True)
-    price = models.CharField(max_length=20, default='', choices=car_price)
     rental_period = models.CharField(max_length=20, choices=rental_period_c, default='')
     rental_option = models.CharField(max_length=15, default='', choices=rental_option_c)
     
@@ -79,9 +161,7 @@ class Car_Rent(models.Model):
 class Car_Sales(models.Model):
     ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    price = models.CharField(max_length=20, default='', choices=car_price)
-    condition = models.CharField(max_length=35, default='', choices=condition_c)
-    kilometer = models.CharField(max_length=35, default='', choices=kilometer_c)
+    kilometer = models.IntegerField(default=0)
     ad_type = models.CharField(max_length=35, default='', choices=sale_or_rent)
 
     def __str__(self):
@@ -91,11 +171,11 @@ class Car_Sales(models.Model):
 class Properties(models.Model):
     bedroom = models.IntegerField()
     bathroom = models.IntegerField()
-    price = models.CharField(max_length=20, default='', choices=properties_price)
     type = models.CharField(max_length=35, default='', choices=properties_type_c)
     furnished = models.BooleanField(default=False)
     compound = models.CharField(max_length=35, default='', choices=compound_c)
-    amenities = models.CharField(max_length=35, default='', choices=amenities_c)
+    amenities = MultiSelectField(choices=amenities_c)
+    
     area = models.IntegerField(default=100)
 
     def __str__(self):
@@ -103,8 +183,8 @@ class Properties(models.Model):
 
 
 class Properties_Rent(models.Model):
-    properties = models.ForeignKey(Properties, on_delete=models.CASCADE)
     ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    properties = models.ForeignKey(Properties, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.ad_id) + ' - ' + str(self.properties)
@@ -123,7 +203,6 @@ class Properties_Sales(models.Model):
 
 class Electronic_ad(models.Model):
     ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
-    price = models.CharField(max_length=15, default='', choices=electronic_price_c)
     type = models.CharField(max_length=35, default='', choices=electronic_type_c)
     condition = models.CharField(max_length=35, default='', choices=condition_c)
     brand = models.CharField(max_length=35, default='', choices=electronic_brand_c)
@@ -142,15 +221,30 @@ class Mobile(models.Model):
 
 class Mobile_ad(models.Model):
     ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
-    modile = models.ForeignKey(Mobile, on_delete=models.CASCADE)
-    price = models.CharField(max_length=15, default='', choices=modile_price_c)
+    mobile = models.ForeignKey(Mobile, on_delete=models.CASCADE)
     condition = models.CharField(max_length=35, default='', choices=condition_c)
     payment = models.CharField(max_length=35, default='', choices=payment_c)
     warranty = models.BooleanField(default=False)
 
     def __str__(self):
-        return str(self.ad_id) + ' - ' + str(self.modile)
+        return str(self.ad_id) + ' - ' + str(self.mobile)
 
+
+class Access_ad(models.Model):
+    ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    condition = models.CharField(max_length=35, default='', choices=condition_c)
+    type = models.CharField(max_length=35, default='', choices=access_type_c)
+
+    def __str__(self):
+        return str(self.ad_id)
+
+class Medical_ad(models.Model):
+    ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    condition = models.CharField(max_length=35, default='', choices=condition_c)
+    type = models.CharField(max_length=35, default='', choices=medical_type_c)
+
+    def __str__(self):
+        return str(self.ad_id)
 
 class Department(models.Model):
     name = models.CharField(max_length=35, default='', choices=department_name_c)
@@ -163,7 +257,6 @@ class Department(models.Model):
 class Furniture_ad(models.Model):
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     ad_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
-    price = models.CharField(max_length=15, default='', choices=furniture_price_c)
     condition = models.CharField(max_length=35, default='', choices=condition_c)
 
     def __str__(self):
